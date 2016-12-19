894b1584b89c53fafce60e83a554bd6ce4ea3136
Miguel Andrade <jmandrade0@gmail.com>
2016-12-19 20:18:27 +0000

    first try at waypoints

--- addon/components/sticky-element.js
+++ addon/components/sticky-element.js
@@ -1,9 +1,38 @@
 import Ember from 'ember';
 import layout from '../templates/components/sticky-element';
-import StickyMixin from 'ember-sticky/mixins/sticky';
-const { Component } = Ember;
+const { Component, isNone } = Ember;
+/* global Waypoint */
 
-export default Component.extend(StickyMixin, {
-  tagName: '',
-  layout
+export default Component.extend({
+  layout,
+
+  stickyOptions: ['direction', 'stuckClass'],
+
+  didInsertElement() {
+    this._super(...arguments);
+    this._sticky = new Waypoint.Sticky(this.buildOptions());
+  },
+
+  buildOptions() {
+    let options = this.getProperties(this.stickyOptions);
+
+    Object.keys(options).forEach((key) => {
+      if (isNone(options[key])) {
+        delete options[key];
+      }
+    });
+
+    options.context = this.element.parentNode;
+    options.element = this.element.querySelector('.sticky-container');
+    options.handler = (direction) => {
+      console.log('handler', direction);
+    };
+
+    return options;
+  },
+
+  willDestroyElement() {
+    this._super(...arguments);
+    this._sticky.destroy();
+  }
 });
--- index.js
+++ index.js
@@ -2,5 +2,25 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-sticky'
+  name: 'ember-sticky',
+
+  options: {
+    nodeAssets: {
+      waypoints: function() {
+        var files = [];
+        var includeWaypoints = this.addonOptions.includeWaypoints;
+        if (includeWaypoints === undefined || includeWaypoints === true) {
+          files.push('jquery.waypoints.js')
+        }
+        files.push('shortcuts/sticky.js');
+
+        return { srcDir: 'lib', import: files };
+      }
+    }
+  },
+
+  included: function(parent) {
+    this.addonOptions = parent.options && parent.options.emberSticky || {};
+    this._super.included.apply(this, arguments);
+  }
 };
